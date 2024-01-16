ba797f6a81c54bae8f21db399ed40adf2dad2b95
Matt Russell <MattRussellUK@gmail.com>
2024-01-16 09:01:46 +0000

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Navbar } from './NavBar.tsx'
+import { Button } from 'react-aria-components'
 
-const App = () => (
+export const App = () => (
   <>
     <Navbar />
+    <div className="flex items-baseline space-x-4 p-4">
+      <Button
+        className="bg-blue-500 hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring focus:ring-blue-300 text-white font-bold py-2 px-4 rounded"
+        onPress={() => undefined}
+      >
+        Sample
+      </Button>
+    </div>
   </>
 )
-
-export default App
