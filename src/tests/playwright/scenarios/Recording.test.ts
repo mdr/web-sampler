ad520f0c7b93586e367065987de99c6c556d8318
Matt Russell <MattRussellUK@gmail.com>
2024-01-31 21:32:40 +0000

    test tweaks

--- src/tests/playwright/scenarios/Recording.test.ts
+++ src/tests/playwright/scenarios/Recording.test.ts
@@ -2,24 +2,27 @@ import { test } from '@playwright/experimental-ct-react'
 import { launchApp } from '../pageObjects/launchApp.tsx'
 import { MAX_RECORDING_DURATION } from '../../../components/capture/captureConstants.ts'
 import { StartRecordingOutcome } from '../../../audio/IAudioRecorder.ts'
+import { MountFunction } from '../types.ts'
+import { CapturePageObject } from '../pageObjects/CapturePageObject.ts'
 
-test('recording audio from the Capture page', async ({ mount }) => {
-  const homePage = await launchApp(mount)
-  const capturePage = await homePage.navbar.clickCapture()
-
-  await capturePage.pressRecordButton()
-
-  await capturePage.setVolume(50)
-  await capturePage.expectVolumeMeterToShowLevel(50)
+test('a captured audio file should be shown after recording', async ({ mount }) => {
+  const capturePage = await launchAndStartRecordingOnCapturePage(mount)
 
   await capturePage.pressStopButton()
+
   await capturePage.expectAudioElementToBeShown()
 })
 
+test('a volume meter should indicate audio level during recording', async ({ mount }) => {
+  const capturePage = await launchAndStartRecordingOnCapturePage(mount)
+
+  await capturePage.setVolume(50)
+
+  await capturePage.expectVolumeMeterToShowLevel(50)
+})
+
 test('recording should stop automatically after 20 seconds', async ({ mount }) => {
-  const homePage = await launchApp(mount)
-  const capturePage = await homePage.navbar.clickCapture()
-  await capturePage.pressRecordButton()
+  const capturePage = await launchAndStartRecordingOnCapturePage(mount)
   await capturePage.expectStopButtonToBeShown()
 
   await capturePage.wait(MAX_RECORDING_DURATION)
@@ -27,7 +30,7 @@ test('recording should stop automatically after 20 seconds', async ({ mount }) =
   await capturePage.expectAudioElementToBeShown()
 })
 
-test('an error toast should be shown if no audio is available on selected source', async ({ mount }) => {
+test('an error toast should be shown if no audio is available on the selected source', async ({ mount }) => {
   const homePage = await launchApp(mount)
   const capturePage = await homePage.navbar.clickCapture()
 
@@ -35,3 +38,10 @@ test('an error toast should be shown if no audio is available on selected source
 
   await capturePage.expectToastToBeShown('No audio available in selected input')
 })
+
+const launchAndStartRecordingOnCapturePage = async (mount: MountFunction): Promise<CapturePageObject> => {
+  const homePage = await launchApp(mount)
+  const capturePage = await homePage.navbar.clickCapture()
+  await capturePage.pressRecordButton()
+  return capturePage
+}
