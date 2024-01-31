9985fe79393fafd7568af746a867d20b31611005
Matt Russell <MattRussellUK@gmail.com>
2024-01-31 20:26:01 +0000

    fix test

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom/vitest'
+
+// patch URL.createObjectURL as not supported in jsdom
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+window.URL.createObjectURL = () => 'blob:https://fake-audio'
