e23849801b897d168f765aa2f56f2a7c9f3e3e22
Matt Russell <MattRussellUK@gmail.com>
2024-02-05 08:38:46 +0000

    delete unused test file

--- src/tests/playwright/testData/testBlob.ts
+++
@@ -1,9 +0,0 @@
-import audioUrl from '../../../assets/captured-audio-example.webm'
-
-export const fetchBlob = async (): Promise<Blob> => {
-  const response = await fetch(audioUrl)
-  if (!response.ok) {
-    throw new Error('Audio file could not be loaded')
-  }
-  return await response.blob()
-}
