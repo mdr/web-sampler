bbd416abc07753ed2322415c6e206e5c74b9ba78
Matt Russell <MattRussellUK@gmail.com>
2024-02-21 14:03:10 +0000

    request notification perm

--- src/components/shared/StorageWarningButton.tsx
+++ src/components/shared/StorageWarningButton.tsx
@@ -1,73 +1,29 @@
-import { Button as RacButton, Dialog, DialogTrigger, Heading, Modal, ModalOverlay } from 'react-aria-components'
+import { Button as RacButton, DialogTrigger, Modal, ModalOverlay } from 'react-aria-components'
 import Icon from '@mdi/react'
 import { mdiAlert } from '@mdi/js'
-import { Button, ButtonVariant } from './Button.tsx'
-import { useStorageManagerActions } from '../../storage/storageManagerHooks.ts'
-import { fireAndForget } from '../../utils/utils.ts'
-import { toast } from 'react-toastify'
+import { StorageWarningDialog } from './StorageWarningDialog.tsx'
 
-export const StorageWarningButton = () => {
-  // const { isStoragePersistent } = useStorageManagerState()
-  const storageManagerActions = useStorageManagerActions()
-  return (
-    <DialogTrigger>
-      <RacButton>
-        <Icon path={mdiAlert} size={1} title="Warning" className="hover:text-gray-300" />
-      </RacButton>
-      <ModalOverlay
-        className={({ isEntering, isExiting }) => `
+export const StorageWarningButton = () => (
+  <DialogTrigger>
+    <RacButton>
+      <Icon path={mdiAlert} size={1} title="Warning" className="hover:text-gray-300" />
+    </RacButton>
+    <ModalOverlay
+      className={({ isEntering, isExiting }) => `
             fixed inset-0 z-10 flex min-h-full items-center justify-center overflow-y-auto bg-black/25 p-4 text-center backdrop-blur
             ${isEntering ? 'animate-in fade-in duration-300 ease-out' : ''}
             ${isExiting ? 'animate-out fade-out duration-200 ease-in' : ''}
           `}
-      >
-        <Modal
-          className={({ isEntering, isExiting }) => `
+    >
+      <Modal
+        className={({ isEntering, isExiting }) => `
               w-full max-w-md overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl
               ${isEntering ? 'animate-in zoom-in-95 duration-300 ease-out' : ''}
               ${isExiting ? 'animate-out zoom-out-95 duration-200 ease-in' : ''}
             `}
-        >
-          <Dialog className="relative outline-none">
-            {({ close }) => {
-              const handleAttemptToMakePersistent = () => {
-                fireAndForget(async () => {
-                  const isStoragePersistent = await storageManagerActions.attemptToMakeStoragePersistent()
-                  if (isStoragePersistent) {
-                    toast.info('Storage is now persistent. Your recordings are safe in local storage.')
-                    close()
-                  } else {
-                    toast.error('Unable to make storage persistent.')
-                  }
-                })
-              }
-              return (
-                <>
-                  <Heading slot="title" className="text-xxl my-0 font-semibold leading-6 text-slate-700">
-                    Unable to Make Storage Persistent
-                  </Heading>
-                  <Icon
-                    path={mdiAlert}
-                    size={1}
-                    title="Warning"
-                    className="absolute right-0 top-0 h-6 w-6 text-yellow-400"
-                  />
-                  <p className="mt-3 text-slate-500">
-                    Your browser has not granted permission to make storage persistent. This means that your recordings
-                    may not be 100% safe in local storage.
-                  </p>
-                  <div className="mt-6 flex justify-center">
-                    <Button label="Attempt to make persistent" onPress={handleAttemptToMakePersistent} />
-                  </div>
-                  <div className="mt-6 flex justify-end">
-                    <Button variant={ButtonVariant.PRIMARY} label="Close" onPress={close} />
-                  </div>
-                </>
-              )
-            }}
-          </Dialog>
-        </Modal>
-      </ModalOverlay>
-    </DialogTrigger>
-  )
-}
+      >
+        <StorageWarningDialog />
+      </Modal>
+    </ModalOverlay>
+  </DialogTrigger>
+)
---
+++ src/components/shared/StorageWarningDialog.tsx
@@ -0,0 +1,51 @@
+import { fireAndForget } from '../../utils/utils.ts'
+import { toast } from 'react-toastify'
+import { Dialog, Heading } from 'react-aria-components'
+import Icon from '@mdi/react'
+import { mdiAlert } from '@mdi/js'
+import { Button, ButtonVariant } from './Button.tsx'
+import { useStorageManagerActions } from '../../storage/storageManagerHooks.ts'
+
+export const StorageWarningDialog = () => {
+  const storageManagerActions = useStorageManagerActions()
+  return (
+    <Dialog className="relative outline-none">
+      {({ close }) => {
+        const handleAttemptToMakePersistent = () => {
+          fireAndForget(async () => {
+            const notificationPermission = await Notification.requestPermission()
+            if (notificationPermission !== 'granted') {
+              toast.error('You need to grant notification permission to make storage persistent.')
+              return
+            }
+            const isStoragePersistent = await storageManagerActions.attemptToMakeStoragePersistent()
+            if (isStoragePersistent) {
+              toast.info('Storage is now persistent. Your recordings are safe in local storage.')
+              close()
+            } else {
+              toast.error('Unable to make storage persistent.')
+            }
+          })
+        }
+        return (
+          <>
+            <Heading slot="title" className="text-xxl my-0 font-semibold leading-6 text-slate-700">
+              Unable to Make Storage Persistent
+            </Heading>
+            <Icon path={mdiAlert} size={1} title="Warning" className="absolute right-0 top-0 h-6 w-6 text-yellow-400" />
+            <p className="mt-3 text-slate-500">
+              Your browser has not granted permission to make storage persistent. This means that your recordings may
+              not be 100% safe in local storage.
+            </p>
+            <div className="mt-6 flex justify-center">
+              <Button label="Attempt to make persistent" onPress={handleAttemptToMakePersistent} />
+            </div>
+            <div className="mt-6 flex justify-end">
+              <Button variant={ButtonVariant.PRIMARY} label="Close" onPress={close} />
+            </div>
+          </>
+        )
+      }}
+    </Dialog>
+  )
+}
