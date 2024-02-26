c283da8e961749cfbc0a7436369b4976f1482466
Matt Russell <MattRussellUK@gmail.com>
2024-02-26 17:41:33 +0000

    avoid provider cascade

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,28 +11,32 @@ import { AudioPlayerContext } from '../audioPlayer/AudioPlayerContext.ts'
 import { StorageManagerContext } from '../storage/StorageManagerContext.ts'
 import { WebStorageManager } from '../storage/StorageManager.tsx'
 import { AppConfig } from '../config/AppConfig.ts'
+import reactArrayToTree from 'react-array-to-tree'
 
 export interface AppProps {
   config: AppConfig
 }
 
-export const App = ({ config }: AppProps) => {
+const nestProviders = (config: AppConfig) => {
   const { audioRecorder, audioContextProvider, audioPlayer, soundLibrary } = config
+  return reactArrayToTree([
+    <AudioContextProviderContext.Provider value={audioContextProvider} />,
+    <AudioRecorderContext.Provider value={audioRecorder} />,
+    <AudioPlayerContext.Provider value={audioPlayer} />,
+    <SoundLibraryContext.Provider value={soundLibrary} />,
+    <StorageManagerContext.Provider value={new WebStorageManager()} />,
+  ])
+}
+
+export const App = ({ config }: AppProps) => {
+  const AllProviders = nestProviders(config)
   return (
     <React.StrictMode>
       <ErrorBoundary fallback={<ErrorFallback />}>
         <ToastContainer position="top-center" hideProgressBar closeOnClick closeButton={false} />
-        <AudioContextProviderContext.Provider value={audioContextProvider}>
-          <AudioRecorderContext.Provider value={audioRecorder}>
-            <AudioPlayerContext.Provider value={audioPlayer}>
-              <SoundLibraryContext.Provider value={soundLibrary}>
-                <StorageManagerContext.Provider value={new WebStorageManager()}>
-                  <RouterProvider router={router} />
-                </StorageManagerContext.Provider>
-              </SoundLibraryContext.Provider>
-            </AudioPlayerContext.Provider>
-          </AudioRecorderContext.Provider>
-        </AudioContextProviderContext.Provider>
+        <AllProviders config={config}>
+          <RouterProvider router={router} />
+        </AllProviders>
       </ErrorBoundary>
     </React.StrictMode>
   )
