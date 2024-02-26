a44ca80ae6dd9e66b5386b003b49aa9ad49804d3
Matt Russell <MattRussellUK@gmail.com>
2024-02-26 18:49:10 +0000

    better explanation for storage notification permission

--- src/components/soundsEditorPage/navbar/StorageWarningDialog.tsx
+++ src/components/soundsEditorPage/navbar/StorageWarningDialog.tsx
@@ -2,7 +2,7 @@ import { fireAndForget } from '../../../utils/utils.ts'
 import { toast } from 'react-toastify'
 import { Dialog, Heading } from 'react-aria-components'
 import Icon from '@mdi/react'
-import { mdiAlert } from '@mdi/js'
+import { mdiAlert, mdiDatabaseLock } from '@mdi/js'
 import { Button, ButtonVariant } from '../../shared/Button.tsx'
 import { useStorageManagerActions } from '../../../storage/storageManagerHooks.ts'
 import { isChromiumBasedBrowser } from '../../../utils/browserUtils.ts'
@@ -34,16 +34,26 @@ export const StorageWarningDialog = () => {
         }
         return (
           <>
-            <Heading slot="title" className="text-xxl my-0 font-semibold leading-6 text-slate-700">
-              Unable to Make Storage Persistent
+            <Heading slot="title" className="my-0 text-lg font-semibold leading-6 text-slate-700">
+              Storage Is Not Persistent
             </Heading>
             <Icon path={mdiAlert} size={1} title="Warning" className="absolute right-0 top-0 h-6 w-6 text-yellow-400" />
             <p className="mt-3 text-slate-500">
-              Your browser has not granted permission to make storage persistent. This means that your recordings may
-              not be 100% safe in local storage.
+              Your browser has not granted permission to make storage persistent. This means that your data in local
+              storage may be deleted at any time.
             </p>
+            {isChromiumBasedBrowser() && (
+              <p className="mt-3 text-slate-500">
+                To make storage persistent, you can grant notification permission. This is the most reliable way to make
+                storage persistent on Chromium-based browsers. This site will not send you any notifications.
+              </p>
+            )}
             <div className="mt-6 flex justify-center">
-              <Button label="Attempt To Make Persistent" onPress={handleAttemptToMakePersistent} />
+              <Button
+                icon={mdiDatabaseLock}
+                label="Attempt To Make Storage Persistent"
+                onPress={handleAttemptToMakePersistent}
+              />
             </div>
             <div className="mt-6 flex justify-end">
               <Button variant={ButtonVariant.PRIMARY} label="Close" onPress={close} />
