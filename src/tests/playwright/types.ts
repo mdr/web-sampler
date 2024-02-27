9c83f57107be0c51ecfa593a248c0c54b849a8dc
Matt Russell <MattRussellUK@gmail.com>
2024-02-27 21:29:02 +0000

    import type only

--- src/tests/playwright/types.ts
+++ src/tests/playwright/types.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/experimental-ct-react'
+import type { test } from '@playwright/experimental-ct-react'
 
 // workaround: https://github.com/microsoft/playwright/issues/27509#issuecomment-1754370996
 export type MountResult = Awaited<ReturnType<Parameters<Parameters<typeof test>[1]>[0]['mount']>>
