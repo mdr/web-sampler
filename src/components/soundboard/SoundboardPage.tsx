0b8cceef4eed01a6934763e5bdab33523f1394c0
Matt Russell <MattRussellUK@gmail.com>
2024-03-18 21:43:34 +0000

    Experiment with react-grid-layout

--- src/components/soundboard/SoundboardPage.tsx
+++ src/components/soundboard/SoundboardPage.tsx
@@ -1,10 +1,49 @@
 import { useSounds } from '../../sounds/soundHooks.ts'
 import { SoundButton } from './SoundButton.tsx'
 import { sortSoundsByDisplayName, soundHasAudio } from '../../types/Sound.ts'
+import GridLayout, { Layout } from 'react-grid-layout'
+import { useMeasure } from 'react-use'
+import { Pixels } from '../../utils/types/brandedTypes.ts'
+
+const EXPERIMENTAL = true
 
 export const SoundboardPage = () => {
   const sounds = sortSoundsByDisplayName(useSounds()).filter(soundHasAudio)
-  return (
+  const columns = 7
+  const [ref, rect] = useMeasure<HTMLDivElement>()
+  const width = Pixels(rect.width)
+
+  const layouts: Layout[] = sounds.map((_sound, index) => ({
+    i: `${index}`,
+    x: index % columns,
+    y: Math.floor(index / columns),
+    w: 1,
+    h: 1,
+    isResizable: false,
+  }))
+  return EXPERIMENTAL ? (
+    <div ref={ref}>
+      <GridLayout
+        layout={layouts}
+        cols={columns}
+        rowHeight={150}
+        width={width}
+        compactType={'horizontal'}
+        isResizable={false}
+        onLayoutChange={(layouts) => {
+          console.log(layouts)
+        }}
+        // autoSize
+        // isBounded
+      >
+        {sounds.map((sound, index) => (
+          <div className="h-[150px] w-[150px] border-2" key={index}>
+            <SoundButton key={sound.id} hotkey={index + 1 + ''} sound={sound} />
+          </div>
+        ))}
+      </GridLayout>
+    </div>
+  ) : (
     <div className="grid grid-cols-[repeat(auto-fill,_150px)] gap-4 p-4">
       {sounds.map((sound, i) => (
         <SoundButton key={sound.id} hotkey={i + 1 + ''} sound={sound} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import { ProdApp } from './components/ProdApp.tsx'
 import { TestApp } from './tests/playwright/TestApp.tsx'
+import { getDocumentRoot } from './utils/domUtils.ts'
 import 'typeface-roboto'
 import 'react-toastify/dist/ReactToastify.css'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 import './main.css'
-import { getDocumentRoot } from './utils/domUtils.ts'
 
 // Set to try the TestApp (used in component tests) when running with yarn dev
 const useTestApp: boolean = import.meta.env.VITE_USE_TEST_APP === 'true'
