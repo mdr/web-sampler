0953234b740a23c4bcc8aa3af8d85991e405cff7
Matt Russell <MattRussellUK@gmail.com>
2024-04-28 16:20:28 +0100

    simplify schema

--- src/sounds/AppDb.ts
+++ src/sounds/AppDb.ts
@@ -3,7 +3,7 @@ import { Sound, SoundId } from '../types/Sound.ts'
 import { Soundboard, SoundboardId } from '../types/Soundboard.ts'
 
 // Used for temporary collection names before we stabilise the IndexedDB schema
-const DEV_UUID = 'ef3634c6-10e9-4714-87fd-f73ec5dfefc6'
+const DEV_UUID = '8ada3f65-bd3d-4200-ac54-79b2883e294d'
 
 export class AppDb extends Dexie {
   sounds!: Table<Sound, SoundId>
@@ -11,7 +11,6 @@ export class AppDb extends Dexie {
 
   constructor() {
     super(`web-sampler-${DEV_UUID}`)
-    this.version(1).stores({ sounds: '++id' })
-    this.version(2).stores({ sounds: '++id', soundboards: '++id' })
+    this.version(1).stores({ sounds: '++id', soundboards: '++id' })
   }
 }
