f5e8220d8e54ad9ccd684066a9a248c4b7b7ba3b
Matt Russell <MattRussellUK@gmail.com>
2024-05-16 20:56:00 +0100

    tidy mp3

--- src/utils/mp3.ts
+++ src/utils/mp3.ts
@@ -1,16 +1,14 @@
 import { AudioData } from '../types/AudioData.ts'
 import { Mp3Encoder } from '@breezystack/lamejs'
+import { Pcm } from './types/brandedTypes.ts'
 
 export const pcmToMp3Blob = (audioData: AudioData): Blob => {
   const mp3Encoder = new Mp3Encoder(1, audioData.sampleRate, 128)
-  const samples = new Int16Array(audioData.pcm.length)
-  for (let i = 0; i < audioData.pcm.length; i++) {
-    samples[i] = audioData.pcm[i] * 0x7fff
-  }
+  const pcm16 = to16BitPcm(audioData.pcm)
   const sampleBlockSize = 1152 //can be anything but make it a multiple of 576 to make encoders life easier
   const mp3Data = []
-  for (let i = 0; i < samples.length; i += sampleBlockSize) {
-    const sampleChunk = samples.subarray(i, i + sampleBlockSize)
+  for (let i = 0; i < pcm16.length; i += sampleBlockSize) {
+    const sampleChunk = pcm16.subarray(i, i + sampleBlockSize)
     const mp3buf = mp3Encoder.encodeBuffer(sampleChunk)
     if (mp3buf.length > 0) {
       mp3Data.push(mp3buf)
@@ -18,7 +16,15 @@ export const pcmToMp3Blob = (audioData: AudioData): Blob => {
   }
   const mp3buf = mp3Encoder.flush()
   if (mp3buf.length > 0) {
-    mp3Data.push(new Int8Array(mp3buf))
+    mp3Data.push(mp3buf)
   }
   return new Blob(mp3Data, { type: 'audio/mp3' })
+}
+
+const to16BitPcm = (pcm: Pcm): Int16Array => {
+  const pcm16 = new Int16Array(pcm.length)
+  for (let i = 0; i < pcm.length; i++) {
+    pcm16[i] = pcm[i] * 0x7fff
+  }
+  return pcm16
 }
