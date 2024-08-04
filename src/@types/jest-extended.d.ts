5e5997f0049c56948aedbed3112ef61b875d4214
Matt Russell <MattRussellUK@gmail.com>
2024-08-04 20:45:31 +0100

    bump typescript-eslint

--- src/@types/jest-extended.d.ts
+++ src/@types/jest-extended.d.ts
@@ -3,12 +3,12 @@ import 'vitest'
 
 // https://jest-extended.jestcommunity.dev/docs/getting-started/setup#vitest-typescript-types-setup
 declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-object-type
   interface Assertion<T = any> extends CustomMatchers<T> {}
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-object-type
   interface AsymmetricMatchersContaining<T = any> extends CustomMatchers<T> {}
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface ExpectStatic extends CustomMatchers<T> {}
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,8 +7,6 @@ export const fireAndForget = <T>(fn: () => Promise<T>): void => unawaited(fn())
 
 export const doNothing = (): void => undefined
 
-export const todo = <T>(): T => undefined as unknown as T
-
 export const concatenateFloat32Arrays = (arrays: Float32Array[]): Float32Array => {
   const totalLength = _.sumBy(arrays, (buffer) => buffer.length)
   const combinedBuffer = new Float32Array(totalLength)
