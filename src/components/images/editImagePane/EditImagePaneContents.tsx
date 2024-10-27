8017ad47b1d56ebe8ec242686507908356accd19
Matt Russell <MattRussellUK@gmail.com>
2024-10-27 17:47:33 +0000

    wip

--- src/components/images/editImagePane/EditImagePaneContents.tsx
+++ src/components/images/editImagePane/EditImagePaneContents.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Crop, ReactCrop } from 'react-image-crop'
+import { ReactEventHandler, useEffect, useState } from 'react'
+import { Crop, ReactCrop, centerCrop, makeAspectCrop } from 'react-image-crop'
 
 import { useImage } from '../../../sounds/library/imageHooks.ts'
 import { useSoundActions } from '../../../sounds/library/soundHooks.ts'
@@ -20,6 +20,25 @@ export const EditImagePaneContents = ({ imageId }: EditImagePaneContentsProps) =
   const setImageName = (name: string) => soundActions.setImageName(imageId, name)
   const [imageUrl, setImageUrl] = useState<Option<Url>>(undefined)
   const imageData = image.data
+  const onImageLoad: ReactEventHandler<HTMLImageElement> = (e) => {
+    const { naturalWidth: width, naturalHeight: height } = e.currentTarget
+    const crop = centerCrop(
+      makeAspectCrop(
+        {
+          unit: 'px',
+          height: 50,
+          width: 50,
+        },
+        1,
+        width,
+        height,
+      ),
+      width,
+      height,
+    )
+    setCrop(crop)
+  }
+
   useEffect(() => {
     if (imageData !== undefined) {
       const blob = new Blob([imageData.bytes], { type: imageData.mediaType })
@@ -30,17 +49,26 @@ export const EditImagePaneContents = ({ imageId }: EditImagePaneContentsProps) =
       setImageUrl(undefined)
     }
   }, [imageData])
-
   return (
     <div className="flex flex-col space-y-4 px-4 pt-4">
       <ImageNameTextField name={image.name} setName={setImageName} />
       {image.data === undefined && <ImageUploadZone imageId={imageId} />}
       {imageUrl && (
-        <div className="w-full max-h-96 flex justify-center items-center overflow-hidden rounded-lg">
-          <ReactCrop crop={crop} onChange={setCrop}>
-            <img src={imageUrl} alt={getImageDisplayName(image)} className="max-w-full max-h-96 rounded-lg" />
-          </ReactCrop>
-        </div>
+        <ReactCrop
+          className="w-full h-full object-contain rounded-lg"
+          crop={crop}
+          onChange={setCrop}
+          aspect={1}
+          minWidth={32}
+          minHeight={32}
+        >
+          <img
+            src={imageUrl}
+            alt={getImageDisplayName(image)}
+            className="max-w-full max-h-96 rounded-lg"
+            onLoad={onImageLoad}
+          />
+        </ReactCrop>
       )}
     </div>
   )
