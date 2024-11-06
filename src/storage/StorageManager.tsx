01555160903bf592031e9dca23701ad3631c7be1
Matt Russell <MattRussellUK@gmail.com>
2024-11-06 22:40:47 +0000

    storage manager tweaks

--- src/storage/StorageManager.tsx
+++ src/storage/StorageManager.tsx
@@ -16,19 +16,17 @@ export class StorageManager extends AbstractService<StorageManagerState> impleme
     super({ isStoragePersistent: false })
   }
 
-  checkIfStorageIsPersistent = async (): Promise<void> => {
-    const isStoragePersistent = await navigator.storage.persisted()
-    this.setState({ isStoragePersistent })
-  }
+  checkIfStorageIsPersistent = async (): Promise<void> =>
+    this.setState({ isStoragePersistent: await navigator.storage.persisted() })
+
+  private requestNotificationPermission = async (): Promise<boolean> =>
+    (await Notification.requestPermission()) === 'granted'
 
   attemptToMakeStoragePersistent = async (): Promise<AttemptToMakeStoragePersistentResult> => {
     // On Chromium-based browsers, the most reliable way to make storage persistent is to request
     // notification permission:
-    if (isChromiumBasedBrowser()) {
-      const notificationPermission = await Notification.requestPermission()
-      if (notificationPermission !== 'granted') {
-        return AttemptToMakeStoragePersistentResult.NOTIFICATION_PERMISSION_DENIED
-      }
+    if (isChromiumBasedBrowser() && (await this.requestNotificationPermission())) {
+      return AttemptToMakeStoragePersistentResult.NOTIFICATION_PERMISSION_DENIED
     }
     const isStoragePersistent = await navigator.storage.persist()
     this.setState({ isStoragePersistent })
