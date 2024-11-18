802fff247738ad09c4d6b5a1ba285d92f177b74f
Matt Russell <MattRussellUK@gmail.com>
2024-11-18 22:22:43 +0000

    try new headless chrome

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -54,6 +54,8 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+        // new Chromium headless (https://playwright.dev/docs/release-notes#try-new-chromium-headless):
+        channel: 'chromium',
         launchOptions: {
           args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
         },
